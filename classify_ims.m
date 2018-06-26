000b7e712c9550851edd3b3e9963d24d5a3df293
lisik <sherl0ck>
2018-06-26 16:00:04 -0400

    Added functionality to run multiple layers

--- classify_ims.m
+++ classify_ims.m
@@ -1,13 +1,14 @@
 % code to categorize stimuli based on image features 
-% im_features can be set to pixel intensity (RGB or grayscale) or output of different VGG layers
+% im_features can be set to pixel intensity (RGB or grayscale) or output of
+% different VGG layers (set layers below)
 clear all
 im_dir =  '~/Dropbox (MIT)/Projects/social_interaction/stimuli/MEG_stills/CNN_experiment/';
 % 2 classes of images, in separate directories
 class1_dir = [im_dir '/interaction/'];
 class2_dir = [im_dir '/non-interaction/'];
 
-im_features = 'vgg06'; %'vgg32'; %'gray'; %options: grayscale, rgb, vggNN (where NN is 01-39, corresponding to layer #)  
-% 5 pooling layers in VGG are 06, 11, 18, 25, 32
+im_features = 'vgg'; %'vgg32'; %'gray'; %options: grayscale, rgb, vgg  
+vgg_layers = [6, 11, 18, 25, 32];% 5 pooling layers in VGG are 06, 11, 18, 25, 32
 im_format = '.jpg';
 run_pca = 1;
 num_PCs = 40;
@@ -21,12 +22,19 @@ class1 = {class1.name};
 class2 = dir([class2_dir '*' im_format]);
 class2 = {class2.name};
 
-if strcmp(im_features(1:3), 'vgg')
+if strcmp(im_features, 'vgg')
     % can modify here with AlexNet if preferred
     net = vgg16;
     imageSize = net.Layers(1).InputSize;
+    layers = vgg_layers;
+else
+    layers = 1; %dummy variable for pixel case
 end
+acc= zeros(length(layers),resample_runs);
+D = zeros(length(layers), length(class1)*2, length(class1)*2);
 
+for l = 1:length(layers)
+    clear class1_ims class2_ims
 for i = 1:length(class1)
     
     if strcmpi(im_features, 'gray')
@@ -37,19 +45,16 @@ for i = 1:length(class1)
     tmp1 = imread([class1_dir class1{i}]);
     tmp2 = imread([class2_dir class2{i}]);
     
-    elseif strcmpi(im_features(1:3), 'VGG')
-    try
-    l = str2double(im_features(end-1:end));
-    catch
-    error('last two characters must correspond to layer #, eg 01 for layer 1')
-    end
+    elseif strcmpi(im_features, 'VGG')
+    layer = layers(l);
+  
     tmp1 = imread([class1_dir class1{i}]);
     tmp1 = augmentedImageDatastore(imageSize, tmp1, 'ColorPreprocessing', 'gray2rgb');
-    tmp1 = activations(net, tmp1, l, 'OutputAs', 'columns');
+    tmp1 = activations(net, tmp1, layer, 'OutputAs', 'columns');
     
     tmp2 = imread([class2_dir class2{i}]);
     tmp2 = augmentedImageDatastore(imageSize, tmp2, 'ColorPreprocessing', 'gray2rgb');
-    tmp2 = activations(net, tmp2, l, 'OutputAs', 'columns');
+    tmp2 = activations(net, tmp2, layer, 'OutputAs', 'columns');
    
     else
     error('Not a valid image feature \n options are rgb, gray, VGGNN (where NN is layer #')
@@ -61,7 +66,6 @@ end
 
 imageFeatures = double([class1_ims; class2_ims]);
 labels = [ones(1,length(class1)), 2*ones(1,length(class2))];
-acc= zeros(1,resample_runs);
 
 if run_pca
 imageFeatures_orig = imageFeatures;
@@ -80,11 +84,11 @@ for i = 1:resample_runs
     
     SVMStruct = fitcsvm(train_data,train_labels);
     pred = predict(SVMStruct, test_data);
-    acc(i) = sum(pred==test_labels)/length(test_labels);
+    acc(l,i) = sum(pred==test_labels)/length(test_labels);
     
 end
 
-mean_acc = mean(acc)
-
 %% save RDM
-D = squareform(pdist(imageFeatures));
+D(l,:,:) = squareform(pdist(imageFeatures));
+end
+mean_acc = mean(acc,2)
